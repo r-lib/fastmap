80c21195a10f9c2009b423764478caeacd54d3c8
Winston Chang <winston@stdout.org>
2019-04-16 12:13:01 -0500

    Add tests

---
+++ tests/testthat/test-map.R
@@ -0,0 +1,58 @@
+context("correctness")
+
+
+library(testthat)
+source(test_path("helpers-expect.R"))
+
+test_that("", {
+  m <- fastmap()
+  m$set("asdf", c(1, 2, 3))
+  m$set("foo", "blah")
+
+  expect_equal(m$get("asdf"), c(1, 2, 3))
+  expect_contents_identical(
+    m$as_list(),
+    list("asdf" = c(1, 2, 3), "foo"= "blah")
+  )
+  expect_true(m$exists("asdf"))
+  expect_true(m$exists("foo"))
+  expect_false(m$exists("bar"))
+
+  # Removal
+  m$remove("asdf")
+  expect_equal(m$get("asdf"), NULL) # TODO: return a sentinel value?
+  expect_contents_identical(
+    m$as_list(),
+    list("foo"= "blah")
+  )
+  expect_false(m$exists("asdf"))
+  expect_true(m$exists("foo"))
+  expect_false(m$exists("bar"))
+
+  # Replacement
+  m$set("asdf", list("a", "b"))
+  expect_equal(m$get("asdf"), list("a", "b"))
+  expect_contents_identical(
+    m$as_list(),
+    list("asdf" = list("a", "b"), "foo"= "blah")
+  )
+
+  # NULL handling
+  m$set("asdf", NULL)
+  expect_equal(m$get("asdf"), NULL)
+  expect_true(m$exists("asdf"))
+  expect_contents_identical(
+    m$as_list(),
+    list("asdf" = NULL, "foo"= "blah")
+  )
+})
+
+# =============================================================================
+# Correctness tests for contents_identical
+# =============================================================================
+test_that("contents_identical works", {
+  # The contents_identical function is used by expect_contents_identical. This
+  # checks that it works correctly.
+  expect_false(contents_identical(list(a=NULL, b=NULL), list(a=1, b=2)))
+  expect_false(contents_identical(list(a=1, b=2), list(a=NULL, b=NULL)))
+})
