ab8f3cb580debf27e968bd952a82e5611e72fd9d
Winston Chang <winston@stdout.org>
2019-05-10 20:29:47 -0500

    Check keys for empty string or NA

--- src/fastmap.cpp
+++ src/fastmap.cpp
@@ -25,6 +25,20 @@ extern "C" {
     R_ClearExternalPtr(map_xptr);
   }
 
+  // Note that this returns a const char* which points to the CHARSXP's
+  // memory, so its lifetime must not exceed the CHARSXP's lifetime.
+  const char* key_from_sexp(SEXP key_r) {
+    if (TYPEOF(key_r) != STRSXP || length(key_r) != 1) {
+      error("key must be a one-element character vector");
+    }
+    SEXP key_c = PROTECT(STRING_ELT(key_r, 0));
+    if (key_c == NA_STRING || Rf_StringBlank(key_c)) {
+      error("key must be a string other than NA or \"\"");
+    }
+    UNPROTECT(1);
+    return CHAR(key_c);
+  }
+
 
   SEXP C_map_create() {
     si_map* map = new si_map;
@@ -36,15 +50,12 @@ extern "C" {
 
 
   SEXP C_map_set(SEXP map_xptr, SEXP key_r, SEXP idx_r) {
-    if (TYPEOF(key_r) != STRSXP || length(key_r) != 1) {
-      error("key must be a one-element character vector");
-    }
+    const char* key = key_from_sexp(key_r);
 
     if (TYPEOF(idx_r) != INTSXP || length(idx_r) != 1) {
       error("idx must be a one-element integer vector");
     }
 
-    const char* key = CHAR(STRING_ELT(key_r, 0));
     si_map* map = map_from_xptr(map_xptr);
     int idx = INTEGER(idx_r)[0];
 
@@ -55,12 +66,9 @@ extern "C" {
 
 
   SEXP C_map_get(SEXP map_xptr, SEXP key_r) {
-    if (TYPEOF(key_r) != STRSXP || length(key_r) != 1) {
-      error("key must be a one-element character vector");
-    }
+    const char* key = key_from_sexp(key_r);
 
     si_map* map = map_from_xptr(map_xptr);
-    const char* key = CHAR(STRING_ELT(key_r, 0));
 
     si_map::const_iterator it = map->find(key);
     if (it == map->end()) {
@@ -72,12 +80,9 @@ extern "C" {
 
 
   SEXP C_map_remove(SEXP map_xptr, SEXP key_r) {
-    if (TYPEOF(key_r) != STRSXP || length(key_r) != 1) {
-      error("key must be a one-element character vector");
-    }
+    const char* key = key_from_sexp(key_r);
 
     si_map* map = map_from_xptr(map_xptr);
-    const char* key = CHAR(STRING_ELT(key_r, 0));
 
     si_map::iterator it = map->find(key);
     if (it == map->end()) {
@@ -96,7 +101,7 @@ extern "C" {
 
   SEXP C_map_keys(SEXP map_xptr) {
     si_map* map = map_from_xptr(map_xptr);
-    SEXP keys =  PROTECT(Rf_allocVector(STRSXP, map->size()));
+    SEXP keys = PROTECT(Rf_allocVector(STRSXP, map->size()));
 
     int i = 0;
     for(si_map::const_iterator it = map->begin(); it != map->end(); ++it, ++i) {
--- tests/testthat/test-map.R
+++ tests/testthat/test-map.R
@@ -22,7 +22,7 @@ test_that("General correctness", {
 
   # Removal
   m$remove("asdf")
-  expect_equal(m$get("asdf"), NULL) # TODO: return a sentinel value?
+  expect_equal(m$get("asdf"), NULL)
   expect_mapequal(
     m$as_list(),
     list("foo"= "blah")
@@ -34,7 +34,7 @@ test_that("General correctness", {
   expect_identical(m$size(), length(get_self(m)$values) - get_self(m)$n_holes)
   # Removing non-existent key has no effect
   m$remove("asdf")
-  expect_equal(m$get("asdf"), NULL) # TODO: return a sentinel value?
+  expect_equal(m$get("asdf"), NULL)
 
   # Adding back
   m$set("asdf", list("a", "b"))
@@ -124,5 +124,48 @@ test_that("Missing keys", {
     m$mget(c("a", "b"), missing = NULL),
     list(a = NULL, b = NULL)
   )
+})
 
+
+test_that("Malformed keys", {
+  m <- fastmap()
+  expect_error(m$set(1, 123))
+  expect_error(m$set(TRUE, 123))
+  expect_error(m$set(NA_character_, 123))
+  expect_error(m$set(NA_integer_, 123))
+  expect_error(m$set("", 123))
+  expect_error(m$set(character(0), 123))
+  expect_error(m$set(numeric(0), 123))
+  expect_error(m$set(NULL, 123))
+
+  expect_error(m$get(1))
+  expect_error(m$get(TRUE))
+  expect_error(m$get(NA_character_))
+  expect_error(m$get(NA_integer_))
+  expect_error(m$get(""))
+  expect_error(m$get(character(0)))
+  expect_error(m$get(numeric(0)))
+  expect_error(m$get(NULL))
+
+  expect_mapequal(m$mget(NULL), list()) # Fix this test. Should we allow NULL?
+  expect_error(m$mget(c(1, 2)))
+  expect_error(m$mget(c("A", "")))
+  expect_error(m$mget(c("A", NA)))
+
+  expect_error(m$remove(NA_character_))
+  expect_error(m$remove(NA_integer_))
+  expect_error(m$remove(""))
+  expect_error(m$remove(character(0)))
+  expect_error(m$remove(numeric(0)))
+  expect_error(m$remove(NULL))
+
+  # When removing multiple, one bad key shouldn't stop others from being removed
+  m$mset(a=1, b=2, c=3)
+
+  expect_error(m$remove(c("a", NA, "c")))
+  expect_mapequal(m$as_list(), list(b=2))
+
+  # Key or value unspecified
+  m$set("a")
+  m$set(value = 123)
 })
