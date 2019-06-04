44ab04bbab168e7996cd52089ac494f4ec4d0e29
Winston Chang <winston@stdout.org>
2019-06-03 22:23:47 -0500

    Update script to download robin_map

--- scripts/update_tsl_libs.R
+++ scripts/update_tsl_libs.R
@@ -1,17 +1,17 @@
 #!/usr/bin/env Rscript
-# Retrieves a particular version of tsl/hopscotch_map:
-#  https://github.com/Tessil/hopscotch-map
+# Retrieves a particular version of tsl/robin_map:
+#  https://github.com/Tessil/robin-map
 
 library(rprojroot)
 
-version   <- "2.2.1"
+version   <- "0.6.1"
 tag       <- paste0("v", version)
-zip_file  <- file.path(tempdir(), sprintf("hopscotch-map-%s.zip", tag))
-url       <- sprintf("https://github.com/Tessil/hopscotch-map/archive/%s.zip", tag)
+zip_file  <- file.path(tempdir(), sprintf("robin-map-%s.zip", tag))
+url       <- sprintf("https://github.com/Tessil/robin-map/archive/%s.zip", tag)
 
 download.file(url, zip_file)
 unzip(zip_file, exdir = tempdir())
-src_dir <- file.path(tempdir(), sprintf("hopscotch-map-%s", version))
+src_dir <- file.path(tempdir(), sprintf("robin-map-%s", version))
 
 dest_dir  <- rprojroot::find_package_root_file("src/lib/tsl")
 unlink(dest_dir, recursive = TRUE)
@@ -19,7 +19,7 @@ unlink(dest_dir, recursive = TRUE)
 dir.create(dest_dir, recursive = TRUE)
 file.copy(
   file.path(src_dir, "include", "tsl",
-    c("hopscotch_hash.h", "hopscotch_map.h", "hopscotch_growth_policy.h")
+    c("robin_hash.h", "robin_map.h", "robin_growth_policy.h")
   ),
   dest_dir,
   overwrite = TRUE
