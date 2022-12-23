f81de9193586e5a008da79f6ed1b84eb04513940
Winston Chang <winston@stdout.org>
2022-12-23 11:37:24 -0600

    Formatting

--- tools/update_hopscotch_map.R
+++ tools/update_hopscotch_map.R
@@ -4,7 +4,10 @@ library(rprojroot)
 version <- "2.3.0"
 tag <- paste0("v", version)
 
-source_url_path <- sprintf("https://raw.githubusercontent.com/Tessil/hopscotch-map/%s/include/tsl/", tag)
+source_url_path <- sprintf(
+  "https://raw.githubusercontent.com/Tessil/hopscotch-map/%s/include/tsl/",
+  tag
+)
 
 dest_dir <- find_package_root_file("src/lib/tsl/")
 
